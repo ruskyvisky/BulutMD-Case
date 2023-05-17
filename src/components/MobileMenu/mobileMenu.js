5ac73b291c8b1daae6434f0709c08c970464bdad
ruskyvisky <numan_859@hotmail.com>
2023-05-18 02:17:04 +0300

    FEA: Routing Add for mobile version

--- src/components/MobileMenu/mobileMenu.js
+++ src/components/MobileMenu/mobileMenu.js
@@ -1,15 +1,26 @@
 import React from 'react'
 import MobileMenuItem from '../MobileMenuItem/mobileMenuItem'
+import { useNavigate } from "react-router-dom";
 export default function MobileMenu({visible}) {
+  const navigate = useNavigate();
   if(!visible){
     return null;
 }
   return (
     <div className='bg-black w-56 absolute top-8 left-0 py-5 flex-col border-2 border-gray-800 flex'>
         <div className='flex flex-col gap-4'>
-        <MobileMenuItem label='Home'/>
-        <MobileMenuItem label='Series'/>
-        <MobileMenuItem label='Films'/>
+        <MobileMenuItem label='Home' onClick={()=>{
+            navigate("/")
+          
+        }}/>
+        <MobileMenuItem label='Series'  onClick={()=>{
+            navigate("/series")
+          
+        }}/>
+        <MobileMenuItem label='Films'  onClick={()=>{
+            navigate("/films")
+          
+        }}/>
         </div>
 
     </div>
--- src/components/MobileMenuItem/mobileMenuItem.js
+++ src/components/MobileMenuItem/mobileMenuItem.js
@@ -1,8 +1,8 @@
 import React from 'react'
 
-export default function mobileMenuItem({label}) {
+export default function mobileMenuItem({label , onClick}) {
   return (
-    <div className='px-3 text-center text-white hover:underline'>
+    <div className='px-3 text-center text-white hover:underline' onClick={onClick}>
     {label}
 </div>
   )
