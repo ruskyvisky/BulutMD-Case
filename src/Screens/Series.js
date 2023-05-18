c69da5f380debda093351fa5533849513a418cab
ruskyvisky <numan_859@hotmail.com>
2023-05-18 14:16:11 +0300

    FEA : WatchCard add

--- src/Screens/Series.js
+++ src/Screens/Series.js
@@ -1,7 +1,13 @@
 import React from 'react'
-
+import WatchCard from '../components/WatchCard/watchCard'
 export default function Series() {
   return (
-    <div>Series Page</div>
+    <>
+    <div className="flex flex-wrap justify-center items-center h-screen"> 
+    <WatchCard description={"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."} title={"Jenny's Wedding"} poster={"https://upload.wikimedia.org/wikipedia/en/c/c9/Jenny%27s_Wedding_Poster.jpg" } releaseDate={2013} />
+   
+</div>
+   
+    </>
   )
 }
---
+++ src/components/WatchCard/watchCard.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+const FilmCard = ({ poster, description, title, releaseDate }) => {
+  return (
+    <div className="w-1/2 max-w-xs rounded overflow-hidden shadow-lg p-6">
+      <img src={poster} alt={title} className="w-full" />
+      <div className="px-6 py-4">
+        <div className="font-bold text-white text-xl mb-2">{title}</div>
+        <p className="text-white text-base">{description}</p>
+      </div>
+      <div className="px-6 py-4">
+        <span className="inline-block bg-green-400 rounded-full px-3 py-1 text-sm font-semibold text-white mr-2">
+          {releaseDate}
+        </span>
+      </div>
+    </div>
+  );
+};
+
+export default FilmCard;
